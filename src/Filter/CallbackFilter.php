a2892e2d21ff6f39842bc0c76bc0bff7df9235b8
Yan Bein <rbeinyan@gmail.com>
2022-08-19 08:57:15 -0300

    Fix

--- src/Filter/CallbackFilter.php
+++ src/Filter/CallbackFilter.php
@@ -36,7 +36,7 @@ class CallbackFilter extends BaseFilter
     /**
      * {@inheritdoc}
      */
-    public function getDefaultOptions()
+    public function getDefaultOptions(): array
     {
         return [
             'callback' => null,
@@ -49,7 +49,7 @@ class CallbackFilter extends BaseFilter
     /**
      * {@inheritdoc}
      */
-    public function getRenderSettings()
+    public function getRenderSettings(): array
     {
         return ['sonata_type_filter_default', [
             'field_type' => $this->getFieldType(),
